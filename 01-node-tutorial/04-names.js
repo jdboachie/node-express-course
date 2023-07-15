ce61cdca064ea84eb267de0a73c9e2b4f0ac3968
jdboachie <judeboachiekwame@gmail.com>
2023-07-15 23:00:55 +0000

    completed node tutorial

--- 01-node-tutorial/04-names.js
+++ 01-node-tutorial/04-names.js
@@ -4,4 +4,4 @@ const secret = 'SUPER SECRET'
 const john = 'john'
 const peter = 'peter'
 
-module.exports = { john, peter }
+export default { john, peter }
--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,21 @@
-console.log('Express Tutorial')
+const express = require('express')
+const app = express()
+const {products} = require('./data')
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products/', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product
+        return { id, name, image }
+    })
+    res.json(newProducts)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}....`)
+})
